e756e80305b3f364423ecb9d6e967cb81b0aba81
Paul <Yeem>
2012-10-16 13:24:58 -0700

    learning switch learned! <(^_^<)  <(^_^)^  ^(^_^)^  ^(^_^)>  (>^_^)>

--- learning_switch.py
+++ learning_switch.py
@@ -7,8 +7,15 @@ Create your learning switch in this file.
 class LearningSwitch(Entity):
     def __init__(self):
         # Add your code here!
-        pass
+        self.dst_to_port_map = {}
+        # if we see a packet with new src
 
     def handle_rx (self, packet, port):
         # Add your code here!
-        raise NotImplementedError
+        self.dst_to_port_map[packet.src] = port
+        if (packet.dst in self.dst_to_port_map.keys()):
+            self.send(packet, self.dst_to_port_map[packet.dst])
+        else:
+            # Flood all ports, EXCEPT the one that the packet arrived from
+            self.send(packet, packet.src , True)
+            
