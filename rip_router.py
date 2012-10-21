1c3d064a30dd50ec595893273daaab5ff335bca3
Paul <Yeem>
2012-10-20 19:50:37 -0700

    Update because I have to give the SM a break.

--- rip_router.py
+++ rip_router.py
@@ -26,11 +26,12 @@ class RIPRouter (Entity):
             packetData = packet.__repr__()
             #<%s from %s->%s is restOfData
             restOfData = packetData.split(", ")[0]
-            packet_dst = restOfData.split("->")[1]
-            
+            packet_src = restOfData.split("->")[0].split(" from ")[1]
+            #packet_dst = restOfData.split("->")[1]
             is_link_up = packetData.split(", ")[1][:-1]
             
-            
+            self.distance_table.append((packet_src, 1))
+            self.send_RoutingUpdates()
             
         elif isinstance(packet, RoutingUpdate):
             # HANDLE ROUTING UPDATE
@@ -41,15 +42,24 @@ class RIPRouter (Entity):
             # SEND RoutingUpdates to NEIGHBORS
             # Steps:
             # 1. Need to figure out what paths we can take to all destinations
+            
+            
             # 2. Send those values in RoutingUpdate class to all neighbors
-                
+            self.send_RoutingUpdates()    
         else:
             # FORWARD THE PACKET CORRECTLY
+            
+            # FROM PIAZZA: If your router does not have a route for a particular destination -- Drop that packet!
             #self.send(packet, SOURCE, )
             
         
             raise NotImplementedError
 
     def send_RoutingUpdates(self):
+        newRoutingUpdate = RoutingUpdate(PACKET)
+        for(dest, distance in SOME MAPPING):
+            newRoutingUpdate.add_destination(dest, distance)
+            
+        
         
         raise NotImplementedError
